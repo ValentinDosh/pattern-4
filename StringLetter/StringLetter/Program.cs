d2a8dbb913c43ee8ea6d44e0da3a28473da50ef9
Valentin <harrypotter9173656@gmail.com>
2018-01-22 23:29:19 +0300

    Task1 for Cheking

---
+++ StringLetter/StringLetter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICountLetter str = new String("Hello");
+            Console.WriteLine(str.CountLetter('l'));
+
+            str.Value = "Harambara";
+            Console.WriteLine(str.CountLetter('a'));
+        }
+    }
+}
