03dde65c9849441cba92a0b9a22ccf8be9540b12
Valentin <harrypotter9173656@gmail.com>
2018-01-22 23:37:25 +0300

    Task3 for Checking

---
+++ ProductDecorator/ProductDecorator/Bed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDecorator
+{
+    class Bed : Product
+    {
+        public Bed() : base("Кровать", "#125")
+        {
+        }
+
+        public override int GetPrice()
+        {
+            return 780;
+        }
+    }
+}
