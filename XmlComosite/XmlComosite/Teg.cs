3ad37751c091eaff89a620c44532a9e261e07acf
Valentin <harrypotter9173656@gmail.com>
2018-01-22 23:32:02 +0300

    Task2 for Checking

---
+++ XmlComosite/XmlComosite/Teg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlComosite
+{
+    class Teg : Component
+    {
+        public Teg(string name) : base(name)
+        {
+        }
+
+        public Teg(string name, string content) : base(name, content)
+        {
+        }
+    }
+}
