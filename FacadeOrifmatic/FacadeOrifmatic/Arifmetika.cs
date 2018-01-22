a2f773dc1cbfdf896817cf06c375e34fd2b80d94
Valentin <harrypotter9173656@gmail.com>
2018-01-22 23:40:12 +0300

    Task4 for Checking

---
+++ FacadeOrifmatic/FacadeOrifmatic/Arifmetika.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeOrifmatic
+{
+    class Factorial
+    {
+        public static int ResultFactorial(int number)
+        {
+            if (number > 12)
+                return 0;
+
+            if (number == 1)
+                return 1;
+            else
+            {
+                return number * ResultFactorial(number - 1);
+            }
+        }
+    }
+
+    class Logarifm
+    {
+        public static double ResultLogarifm(double number)
+        {
+            return Math.Log10(number);
+        }
+    }
+
+    class Sqrt
+    {
+        public static double ResultSqrt(double number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+
+    class Sum
+    {
+        public static int ResultSum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    class Substraction
+    {
+        public static int ResultSub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    class Multiplication
+    {
+        public static int ResultMulti(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ FacadeOrifmatic/FacadeOrifmatic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeOrifmatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FacadeArifmetic f = new FacadeArifmetic();
+
+            int a = f.GetFactorial(10);
+            double b = f.GetLog(300);
+            int c = f.GetMulti(3,4);
+            double d = f.GetSqrt(9);
+            int e = f.GetSub(100, 99);
+            int f2 = f.GetSum(2,2);
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", a, b, c, d, e, f2);
+            Console.ReadLine();
+        }
+    }
+}
